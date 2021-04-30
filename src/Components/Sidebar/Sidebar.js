8f0aeabbe8777a26364ccbe02b57c1d63a686027
smilegupta <guptamiley3012@gmail.com>
2021-04-30 23:07:53 +0530

    search

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 const Sidebar = () => {
   return (
@@ -16,13 +17,17 @@ const Sidebar = () => {
           <IconButton>
             <ChatIcon />
           </IconButton>
-
           <IconButton>
             <MoreVertIcon />
           </IconButton>
         </div>
       </div>
-      <div className="sidebar_search"></div>
+      <div className="sidebar_search">
+        <div className="sidebar_search_container">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
       <div className="sidebar_chats"></div>
     </div>
   );
