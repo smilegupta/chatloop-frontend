fae2431a7ff41c90b9e4122a72cb48290f9d883b
smilegupta <guptamiley3012@gmail.com>
2021-04-30 23:57:31 +0530

    chat header

--- src/Components/ChatArea/ChatArea.js
+++ src/Components/ChatArea/ChatArea.js
@@ -1,11 +1,41 @@
-import "./ChatArea.css"
+import { Avatar, IconButton } from "@material-ui/core";
+import "./ChatArea.css";
+import { useEffect, useState } from "react";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
 const ChatArea = () => {
-    return (
-        <div>
-            I am chat area
-        </div>
-    )
-}
+  const [seed, setSeed] = useState("");
 
-export default ChatArea
+  // Random Seed
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat_header_info">
+          <h3>Room Name</h3>
+          <p>Last Seen at ...</p>
+        </div>
+        <div className="chat_header_right">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body"></div>
+      <div className="chat_footer"></div>
+    </div>
+  );
+};
+
+export default ChatArea;
