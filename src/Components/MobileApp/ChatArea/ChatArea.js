fb296b8ba1f1b0b7cba24ce8c1226ec5eb8e41ac
smilegupta <guptamiley3012@gmail.com>
2021-05-08 18:46:25 +0530

    mobile

--- src/Components/MobileApp/ChatArea/ChatArea.js
+++ src/Components/MobileApp/ChatArea/ChatArea.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Avatar, IconButton } from "@material-ui/core";
 import "./ChatArea.css";
-import { useEffect, useState, Fragment } from "react";
+import { useEffect, useState, Fragment, useRef } from "react";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { Link } from "react-router-dom";
 import { axiosFun } from "../../../CRUD/axios.config";
@@ -9,16 +9,28 @@ import { getMessages, sendMessage } from "../../../CRUD/queries";
 import ChatBubble from "./ChatBubble";
 
 const ChatArea = ({ match, auth }) => {
-  const [data, setData] = useState(null);
   const [message, setMessage] = useState("");
-
+  const [data, setData] = useState(null);
+  const scrollref = useRef();
   useEffect(() => {
     getChats();
   }, [match.params.roomId]);
 
+  useEffect(() => {
+    scrollref.current?.scrollIntoView({ behaviour: "smooth" });
+  });
+
   const getChats = async () => {
     const res = await axiosFun(getMessages(match.params.roomId));
     setData(res.data.listMessagess.items);
+    auth.currentConversationMessages = {
+      conversationId: match.params.roomId,
+      ...res.data.listMessagess,
+    };
+    auth.setCurrentConversationMessages({
+      conversationId: match.params.roomId,
+      ...res.data.listMessagess,
+    });
   };
 
   const sendMessageFun = async (e) => {
@@ -41,10 +53,9 @@ const ChatArea = ({ match, auth }) => {
       console.error(err);
     }
   };
-
   return (
     <>
-      {data && (
+      {data && data.length > 0 && (
         <div className="mobile_chat">
           <div className="mobile_chat_header">
             <IconButton>
@@ -61,19 +72,6 @@ const ChatArea = ({ match, auth }) => {
             </div>
           </div>
           <div className="mobile_chat_body">
-            {data.map((chat, idx) => (
-              <ChatBubble
-                key={idx}
-                authorName={chat.authorName}
-                message={chat.message}
-                sentAt={chat.sentAt}
-                sender={
-                  chat.authorId === auth.conversations.userId ? true : false
-                }
-              />
-            ))}
-          </div>
-          <div className="mobile_chat_footer">
             {auth.currentConversationMessages !== undefined &&
               auth.currentConversationMessages &&
               auth.currentConversationMessages.items.map((chat, idx) => (
@@ -87,8 +85,9 @@ const ChatArea = ({ match, auth }) => {
                   }
                 />
               ))}
+            <div ref={scrollref}></div>
           </div>
-          <div className="chat_footer">
+          <div className="mobile_chat_footer">
             <form onSubmit={(e) => sendMessageFun(e)}>
               <input
                 type="text"
